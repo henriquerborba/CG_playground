f06fa8f0e7ed8d7fe22928616b5190430c406b69
henriquerborba <henriquedeborba@hotmail.com>
2022-08-23 05:07:02 -0300

    Resolução do exercício

--- src/main.cpp
+++ src/main.cpp
@@ -13,13 +13,29 @@ using namespace std;
 
 int screenWidth = 800, screenHeight = 800; // largura e altura inicial da tela . Alteram com o redimensionamento de tela.
 int mouseX, mouseY;                        // variaveis globais do mouse para poder exibir dentro da render().
+bool direita = true;
+int x1 = 0, x2 = 100;
+int d = 20;
 
 // funcao chamada continuamente. Deve-se controlar o que desenhar por meio de variaveis globais
 // Todos os comandos para desenho na canvas devem ser chamados dentro da render().
 // Deve-se manter essa fun��o com poucas linhas de codigo.
 void render()
 {
-   CV::text(20, 500, "Programa Demo Canvas2D");
+   CV::color(0);
+   if (direita)
+   {
+
+      CV::line(x1, screenHeight / 2, x2, screenHeight / 2);
+      CV::line(x2 - 10, screenHeight / 2 + d, x2, screenHeight / 2);
+      CV::line(x2 - 10, screenHeight / 2 - d, x2, screenHeight / 2);
+   }
+   else
+   {
+      CV::line(x2, screenHeight / 2, x1, screenHeight / 2);
+      CV::line(x1 + 10, screenHeight / 2 + d, x1, screenHeight / 2);
+      CV::line(x1 + 10, screenHeight / 2 - d, x1, screenHeight / 2);
+   }
 }
 
 // funcao chamada toda vez que uma tecla for pressionada.
@@ -30,12 +46,18 @@ void keyboard(int key)
    {
    // seta para esquerda
    case 200:
+      direita = false;
+      x1--;
+      x2--;
       break;
    // seta para cima
    case 201:
       break;
    // seta para direita
    case 202:
+      direita = true;
+      x1++;
+      x2++;
       break;
    // seta para baixo
    case 203:
